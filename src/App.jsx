74583ed290ad5afb144cb11ed522e2a6dade8740
Yko-koshka <legezaanna7@gmail.com>
2024-10-20 13:16:20 +0200

    created Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages';
+
 const App = () => {
-  return <h1 className="text-7xl font-bold underline">Tailwind project</h1>
-}
-export default App
+  return <h1 className="text-7xl font-bold underline">Tailwind project</h1>;
+};
+export default App;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,4 @@
+const HomeLayout = () => {
+  return <h1 className="text-4xl">Home Layout</h1>;
+};
+export default HomeLayout;
