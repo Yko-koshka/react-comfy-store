5112f63e9317332cd887cc6772e9b8e471ca04db
Yko-koshka <legezaanna7@gmail.com>
2024-10-28 14:49:06 +0100

    setup and rendered header component in HomeLayout, added two links - Login and Register

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className="bg-neutral py-2 text-neutral-content">
+      <div className="align-element flex justify-center sm:justify-end">
+        {/*USER*/}
+        {/*LINKS*/}
+        <div className="flex gap-x-6 justify-center items-center"></div>
+        <Link to="/login" className="link link-hover text-xs sm:text-sm pr-8">
+          Sing in / Guest
+        </Link>
+        <Link to="/register" className="link link-hover text-xs sm:text-sm">
+          Create Account
+        </Link>
+      </div>
+    </header>
+  );
+};
+export default Header;
