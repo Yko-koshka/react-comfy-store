781d0424ed0ef5135a83de0bcc0323ea9366498a
Yko-koshka <legezaanna7@gmail.com>
2024-10-29 17:02:45 +0100

    set theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,31 @@ import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const themes = {
+  coffee: 'coffee',
+  valentine: 'valentine',
+};
+
+const getThemeFromLocalStorage = () => {
+  return localStorage.getItem('theme') || themes.coffee;
+};
 
 const Navbar = () => {
-  const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
   const handleTheme = () => {
-    setTheme(!theme);
+    const { valentine, coffee } = themes;
+    const newTheme = theme === valentine ? coffee : valentine;
+
+    setTheme(newTheme);
   };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element">
