44a6aa67020fbe4947281fb0c3bda41297815c0c
Yko-koshka <legezaanna7@gmail.com>
2024-10-30 13:19:11 +0100

    added some text and btn in the hero.jsx file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,21 @@ const carouselImages = [hero1, hero2, hero3, hero4];
 const Hero = () => {
   return (
     <div className="grid lg:grid-cols-2 gap-24 item-center">
-      <div></div>
+      <div>
+        <h1 className="max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt odio
+          vel nihil dolor est perspiciatis. Suscipit voluptates incidunt esse
+          dolorum!
+        </p>
+        <div className="mt-10">
+          <Link to="/products" className="btn btn-primary">
+            Our Products
+          </Link>
+        </div>
+      </div>
       <div className="hidden h-[28rem] lg:carousel carousel-center p-4 space-x-4 bg-neutral rounded-box">
         {carouselImages.map((image) => {
           return (
